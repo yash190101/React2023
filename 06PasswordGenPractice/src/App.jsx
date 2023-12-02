7f362d5d0501235c0c45c5e16504e1378fbb5e5a
yash190101 <yvpatil2001@gmail.com>
2023-12-02 21:18:31 +0530

    practicing react part

--- 06PasswordGenPractice/src/App.jsx
+++ 06PasswordGenPractice/src/App.jsx
@@ -3,54 +3,93 @@ import { useState, useCallback, useEffect ,useRef} from 'react'
 import './App.css'
 
 function App() {
+  // const [length,setLength] = useState(8)
+  // const [numAllowed,setNumAllowed] = useState(false)
+  // const [charAllowed,setCharAllowed] = useState(false)
+  // const [password,setPassword] = useState("")
+  // const [clicked,setClicked] = useState(false)
+  // const buttonStyle = {
+  //   backgroundColor: clicked ? 'green' : 'orange',
+  //   color: '#fff',
+  //   padding: '10px 20px',
+  //   borderRadius: '5px',
+  //   cursor: 'pointer',
+  //   // Add more styles as needed
+  // };
+
+
+
+  // const generatePassword = useCallback(() =>{
+
+  //   let pass = ''
+  //   let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+  //   if(numAllowed) str+="0123456789"
+  //   if(charAllowed) str+="!@#$%^&*(){}[]<>"
+
+  //   for (let i = 0; i < length; i++) {
+      
+  //     let random = Math.floor(Math.random()*str.length + 1)
+  //     pass+=str.charAt(random)
+      
+  //   }
+  //   setPassword(pass)
+  // },[length,numAllowed,charAllowed,setPassword])
+
+
+  // useEffect(()=>{
+  //   generatePassword()
+  //   setClicked(false)
+  // },[length,numAllowed,charAllowed,generatePassword])
+
+  // const copyPassword = useRef()
+  // console.log(copyPassword);
+  // const copyPasswordToClipboard = useCallback(()=>{
+
+  //   copyPassword.current?.select()
+  //   window.navigator.clipboard.writeText(password)
+  //   setClicked(true)
+
+  // },[password])
   const [length,setLength] = useState(8)
-  const [numAllowed,setNumAllowed] = useState(false)
   const [charAllowed,setCharAllowed] = useState(false)
+  const [numAllowed,setNumAllowed] = useState(false)
   const [password,setPassword] = useState("")
-  const [clicked,setClicked] = useState(false)
-  const buttonStyle = {
-    backgroundColor: clicked ? 'green' : 'orange',
-    color: '#fff',
-    padding: '10px 20px',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    // Add more styles as needed
-  };
+  const [button,setButton] = useState(false)
+  
 
-
-
-  const generatePassword = useCallback(() =>{
-
-    let pass = ''
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
-    if(numAllowed) str+="0123456789"
-    if(charAllowed) str+="!@#$%^&*(){}[]<>"
-
-    for (let i = 0; i < length; i++) {
-      
-      let random = Math.floor(Math.random()*str.length + 1)
-      pass+=str.charAt(random)
-      
+  const generatePassword = useCallback(()=>{
+    let pass = ""
+    let usedforPassword = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let numbers = '0123456789'
+    let char = '!@#$%^&'
+    if(charAllowed) usedforPassword+=char
+    if(numAllowed) usedforPassword+=numbers
+    for(let i=0;i<length;i++){
+      let random = Math.floor(Math.random()*usedforPassword.length)
+      pass+=usedforPassword.charAt(random)
     }
     setPassword(pass)
-  },[length,numAllowed,charAllowed,setPassword])
+    
+  },[length,charAllowed,numAllowed,setPassword])
 
 
   useEffect(()=>{
     generatePassword()
-    setClicked(false)
-  },[length,numAllowed,charAllowed,generatePassword])
+    setButton(false)
+  },[length,charAllowed,numAllowed,generatePassword])
 
-  const copyPassword = useRef()
-  console.log(copyPassword);
+  const copyInput = useRef(null)
+
   const copyPasswordToClipboard = useCallback(()=>{
-
-    copyPassword.current?.select()
+    copyInput.current?.select()
     window.navigator.clipboard.writeText(password)
-    setClicked(true)
-
+    setButton(true)
   },[password])
+
+
+
+
 
   return (
     <>
@@ -64,10 +103,11 @@ function App() {
             className=" rounded-lg outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
-            ref={copyPassword}
+            ref={copyInput}
+            
           />
           
-          <button className=' mx-3 rounded-lg px-4 py-2 text-black bg-orange-300' onClick={copyPasswordToClipboard} style={buttonStyle}>{clicked ? 'Copied' : 'Copy'}</button>
+          <button className=' mx-3 rounded-lg px-4 py-2 text-black  bg-orange-500' onClick={copyPasswordToClipboard}>{button?"Copied":"Copy"}</button>
         </div>
 
         <div className='flex text-sm gap-x-2 py-4'>
@@ -77,7 +117,8 @@ function App() {
             min={8}
             max={50}
             value={length}
-            onChange={(e)=>setLength(e.target.value)}
+            className=' cursor-pointer mx-3'
+            onChange={(e)=>{setLength(e.target.value)}}
             
             /><label> Length : {length}</label>
           </div>
@@ -87,13 +128,14 @@ function App() {
               <input type="checkbox" 
               id="number"
               defaultValue={numAllowed}
-              onChange={()=>setNumAllowed((prev)=>!prev)} 
+              onChange={()=>setNumAllowed((value)=>!value)}
+               
               /><label> Number </label>
 
               <input type="checkbox" 
               id="character"
               defaultValue={charAllowed}
-              onChange={()=>setCharAllowed((prev)=>!prev)} 
+              onChange={()=>setCharAllowed((value)=>!value)}
               /><label> character </label>
           </div>
 
